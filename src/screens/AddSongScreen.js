5cf44875f791f490a7a7b25cde0581e2b451a2d6
vijay70139 <vijay70139@gmail.com>
2026-02-18 12:31:48 +0530

    q5 updated

--- src/screens/AddSongScreen.js
+++ src/screens/AddSongScreen.js
@@ -70,7 +70,7 @@ export default function AddSongScreen({ navigation }) {
   };
 
   const STAR_IMAGES = [
-    require('../assets/images/Qoute5.webp'),
+    require('../assets/images/Qoute05.webp'),
     require('../assets/images/Kid3.webp'),
     require('../assets/images/image5.webp'),
   ];
